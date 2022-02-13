18ae149f6cf91d84a5b8069c2efa485e171e7873
Fabrizio (Misto) Milo <mistobaan@gmail.com>
2022-02-13 08:38:30 +0000

    wip

--- modeling_gpt_neox_20b.py
+++ modeling_gpt_neox_20b.py
@@ -6,6 +6,36 @@ import os
 import glob
 
 
+class RotaryEmbedding(torch.nn.Module):
+    """ """
+
+    def __init__(self, dim, base=10000, precision=torch.half):
+        super().__init__()
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float() / dim))
+        self.register_buffer("inv_freq", inv_freq)
+        self.seq_len_cached = None
+        self.cos_cached = None
+        self.sin_cached = None
+        self.precision = precision
+
+    def forward(self, x, seq_dim=1, seq_len=None):
+        if seq_len is None:
+            seq_len = x.shape[seq_dim]
+        if seq_len != self.seq_len_cached:
+            self.seq_len_cached = seq_len
+            t = torch.arange(seq_len, device=x.device).type_as(self.inv_freq)
+            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+            emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
+            if self.precision == torch.bfloat16:
+                emb = emb.float()
+            self.cos_cached = emb.cos()[:, None, None, :]
+            self.sin_cached = emb.sin()[:, None, None, :]
+            if self.precision == torch.bfloat16:
+                self.cos_cached = self.cos_cached.bfloat16()
+                self.sin_cached = self.sin_cached.bfloat16()
+        return self.cos_cached, self.sin_cached
+
+
 class SpatialGatingUnit(nn.Module):
     def __init__(self, neox_args, d_ff, d_attn=None, causal=True, mask_fn=None):
         super().__init__()
@@ -42,6 +72,11 @@ class SpatialGatingUnit(nn.Module):
         return (gate * res).transpose(0, 1)  # [b, s, d] -> [s, b, d]
 
 
+def gpt2_attention_mask_func(attention_scores, ltor_mask):
+    attention_scores.masked_fill_(ltor_mask, -10000.0)
+    return attention_scores
+
+
 class GPTNeoxAttention(nn.Module):
     """
     Self-attention layer takes input with size [b, s, h]
@@ -50,31 +85,26 @@ class GPTNeoxAttention(nn.Module):
 
     def __init__(
         self,
-        neox_args,
-        attention_mask_func,
-        init_method,
-        output_layer_init_method,
+        config,
         layer_number,
     ):
         super().__init__()
 
-        self.fp16 = neox_args.precision == "fp16"
-        self.bf16 = neox_args.precision == "bfloat16"
-
-        self.attention_mask_func = attention_mask_func
-        self.apply_query_key_layer_scaling = neox_args.apply_query_key_layer_scaling
-        self.attention_softmax_in_fp32 = neox_args.attention_softmax_in_fp32
-        if self.apply_query_key_layer_scaling:
-            self.attention_softmax_in_fp32 = True
+        self.attention_mask_func = gpt2_attention_mask_func
+        # self.apply_query_key_layer_scaling = config.apply_query_key_layer_scaling
+        # self.attention_softmax_in_fp32 = config.attention_softmax_in_fp32
+        # if self.apply_query_key_layer_scaling:
+        #     self.attention_softmax_in_fp32 = True
         self.layer_number = layer_number
         # Per attention head and per partition values.
-        self.pos_emb = neox_args.pos_emb
+        self.pos_emb = config.pos_emb
+        return
 
         # Strided linear layer.
         self.query_key_value = mpu.ColumnParallelLinear(
-            neox_args=neox_args,
-            input_size=neox_args.hidden_size,
-            output_size=3 * neox_args.hidden_size,
+            neox_args=config,
+            input_size=config.hidden_size,
+            output_size=3 * config.hidden_size,
             gather_output=False,
             init_method=init_method,
         )
@@ -85,28 +115,21 @@ class GPTNeoxAttention(nn.Module):
             coeff = max(1, self.layer_number)
             self.norm_factor *= coeff
 
-        if neox_args.rotary_pct == 1:
-            self.rotary_ndims = None
-        else:
-            assert neox_args.rotary_pct < 1
+            assert config.rotary_pct < 1
             self.rotary_ndims = int(
-                self.hidden_size_per_attention_head * neox_args.rotary_pct
+                self.hidden_size_per_attention_head * config.rotary_pct
             )
-        dim = (
-            self.rotary_ndims
-            if self.rotary_ndims is not None
-            else self.hidden_size_per_attention_head
-        )
+        dim = self.rotary_ndims
         self.rotary_emb = RotaryEmbedding(
-            dim, base=neox_args.rotary_emb_base, precision=neox_args.params_dtype
+            dim, base=config.rotary_emb_base, precision=config.params_dtype
         )
 
-        self.attention_type = neox_args.attention_config[layer_number]
-        self.sparse = self.attention_type != "global"
+        self.attention_type = config.attention_config[layer_number]
+
         self.scale_mask_softmax = FusedScaleMaskSoftmax(
             input_in_fp16=self.fp16,
             input_in_bf16=self.bf16,
-            fusion_type=get_fusion_type(neox_args),
+            fusion_type=get_fusion_type(config),
             mask_func=self.attention_mask_func,
             softmax_in_fp32=self.attention_softmax_in_fp32,
             scale=coeff,
@@ -115,13 +138,13 @@ class GPTNeoxAttention(nn.Module):
         # Dropout. Note that for a single iteration, this layer will generate
         # different outputs on different number of parallel partitions but
         # on average it should not be partition dependent.
-        self.attention_dropout = nn.Dropout(neox_args.attention_dropout)
+        self.attention_dropout = nn.Dropout(config.attention_dropout)
 
         # Output.
         self.dense = mpu.RowParallelLinear(
-            neox_args=neox_args,
-            input_size=neox_args.hidden_size,
-            output_size=neox_args.hidden_size,
+            neox_args=config,
+            input_size=config.hidden_size,
+            output_size=config.hidden_size,
             input_is_parallel=True,
             init_method=output_layer_init_method,
             skip_bias_add=True,
@@ -174,11 +197,9 @@ class GPTNeoxAttention(nn.Module):
         # Update attention mask for inference. [b, np, sq, sk]
         # ==================================================
 
-        if self.use_cache:
-            with torch.no_grad():
-                attention_mask = attention_mask[
-                    ..., : attention_scores.size(3), : attention_scores.size(3)
-                ]
+        attention_mask = attention_mask[
+            ..., : attention_scores.size(3), : attention_scores.size(3)
+        ]
 
         # ===========================
         # Attention probs and dropout
@@ -189,8 +210,8 @@ class GPTNeoxAttention(nn.Module):
 
         # This is actually dropping out entire tokens to attend to, which might
         # seem a bit unusual, but is taken from the original Transformer paper.
-        with mpu.get_cuda_rng_tracker().fork():
-            attention_probs = self.attention_dropout(attention_probs)
+        # with mpu.get_cuda_rng_tracker().fork():
+        #   attention_probs = self.attention_dropout(attention_probs)
 
         # =========================
         # Context layer. [sq, b, hp]
@@ -320,31 +341,44 @@ class GPTNeoxAttention(nn.Module):
 
 
 class GPTNeox20BModelConfig:
-    vocab_size = "xx"
-    max_position_embeddings = 2048
-    hidden_size = "xx"
+    max_position_embeddings: int = 2048
     num_hidden_layers = 44
     attention_config = [[["global"], 44]]
     layer_norm_epsilon: float = 1.0e-5
+    attention_dropout: float = 0.0
+    gpt_j_residual: bool = True
+    hidden_dropout: float = 0.0
+    hidden_size: int = 6144
+    make_vocab_size_divisible_by: int = 256
+    no_load_rng: bool = True
+    no_weight_tying: bool = True
+    norm: str = "layernorm"
+    num_attention_heads: int = 64
+    num_layers: int = 44
+    partition_activations: bool = False
+    pos_emb: str = "rotary"
+    rotary_pct: float = 0.25
+    seq_length: int = 2048
+    split: List[int] = [995, 4, 1]
+    tokenizer_type: str = "HFTokenizer"
+    fp16: bool = True
+    vocab_size: int = 54031
 
 
 class GPTNeoxBlock(nn.Module):
-    def __init__(self, config, layer_idx: int):
-        super(self).__init__()
+    def __init__(self, config, layer_number: int):
+        super().__init__()
         hidden_size = config.hidden_size
         self.input_layer_norm = nn.LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
-        self.attn = GPTNeoxAttention(config, layer_idx=layer_idx)
+        self.attn = GPTNeoxAttention(config, layer_number=layer_number)
         self.post_attention_layernorm = nn.LayerNorm(
             hidden_size, eps=config.layer_norm_epsilon
         )
 
-        if config.add_cross_attention:
-            self.crossattention = GPT2Attention(config, is_cross_attention=True)
-            self.ln_cross_attn = nn.LayerNorm(
-                hidden_size, eps=config.layer_norm_epsilon
-            )
+        # self.crossattention = GPTNeoxAttention(config, is_cross_attention=True)
+        # self.ln_cross_attn = nn.LayerNorm(hidden_size, eps=config.layer_norm_epsilon)
 
-        self.mlp = GPT2MLP(inner_dim, config)
+        # self.mlp = GPT2MLP(inner_dim, config)
 
     def forward(self, x, attention_mask):
         x = self.norm(x)
@@ -355,30 +389,82 @@ class GPTNeoxBlock(nn.Module):
         return x, attention_mask
 
 
+def _pre_transformer_block(args):
+    # data format change for hidden_states to avoid explicit tranposes : [b s h] --> [s b h]
+    assert len(args) == 2, "Incorrect number of arguments to _pre_transformer_block"
+    fn = lambda _args: (_args[0].transpose(0, 1).contiguous(), *_args[1:])
+    return fn(args)
+
+
+def _post_transformer_block(args):
+    # from (hidden_states, attention_mask)
+    # to (hidden_states.T)
+    assert len(args) == 2, "Incorrect number of arguments to _post_transformer_block"
+    fn = lambda _args: (_args[0].transpose(0, 1).contiguous())
+    return fn(args)
+
+
 class GPTNeox20BModel(nn.Module):
     def __init__(self, config: GPTNeox20BModelConfig):
         super().__init__()
 
         self.embed_dim = config.hidden_size
+        self.vocab_size = config.vocab_size
 
-        self.wte = nn.Embedding(config.vocab_size, self.embed_dim)
-        self.wpe = nn.Embedding(config.max_position_embeddings, self.embed_dim)
+        # weight token embedding
+        self.word_embeddings_weight = nn.Embedding(self.vocab_size, self.embed_dim)
+        # wight position embedding
+        self.wpe = RotaryEmbedding(self.embed_dim)
 
-        self.drop = nn.Dropout(config.embd_pdrop)
-        self.h = nn.ModuleList(
-            [GPTNeoxBlock(config, layer_idx=i) for i in range(config.num_hidden_layers)]
+        self.drop = nn.Dropout(config.hidden_dropout)
+        self.tranformer = nn.ModuleList(
+            # [_pre_transformer_block]
+            [
+                GPTNeoxBlock(config, layer_number=i)
+                for i in range(config.num_hidden_layers)
+            ]
+            # + [_post_transformer_block]
         )
         self.ln_f = nn.LayerNorm(self.embed_dim, eps=config.layer_norm_epsilon)
 
+        self.head = nn.Linear(self.embed_dim, self.vocab_size, bias=False)
+
     def forward(self, input_ids, position_ids, attention_mask):
-        pass
+        b, t = input_ids.size()
+        assert t <= self.block_size, "Cannot forward, model block size is exhausted."
+
+        token_embeddings = self.word_embeddings_weight(input_ids)
+        x = self.rotary_embeddings(token_embeddings)
+        x = self.drop(x)
+        x = self.transformer(x)
+        x = self.ln_f(x)
+        logits = self.head(x)
+        return logits
+
+
+def vocab_size_with_padding(config, orig_vocab_size):
+    """Pad vocab size so it is divisible by model parallel size and
+    still having GPU friendly size."""
+
+    after = orig_vocab_size
+    multiple = config.make_vocab_size_divisible_by
+    while (after % multiple) != 0:
+        after += 1
+    print(
+        " > padded vocab (size: {}) with {} dummy tokens "
+        "(new size: {})".format(orig_vocab_size, after - orig_vocab_size, after),
+        flush=True,
+    )
+    return after
 
 
 def load_checkpoint(load_path):
     print(load_path)
     assert os.path.exists(load_path)
+    tokenizer = Tokenizer.from_file("./20_checkpoints_merged/20B_tokenizer.json")
 
     config = GPTNeox20BModelConfig()
+    config.vocab_size = vocab_size_with_padding(config, tokenizer.get_vocab_size())
 
     model = GPTNeox20BModel(config)
     model.to(torch.device("cpu"))
@@ -392,10 +478,4 @@ def load_checkpoint(load_path):
 
 
 if __name__ == "__main__":
-    tokenizer = Tokenizer.from_file("./20B_checkpoints/20B_tokenizer.json")
-
-    encoded = tokenizer.encode("test")
-    original = tokenizer.decode(encoded.ids)
-    print(original)
-
-    load_checkpoint("20B_checkpoints/global_step150000/")
+    load_checkpoint("20_checkpoints_merged/global_step150000/")
